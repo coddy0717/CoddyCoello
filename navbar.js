a966241d0bdd9b07e26576a275f6b65ed3d69adc
coddy0717 <86177840+coddy0717@users.noreply.github.com>
2021-07-02 23:16:41 -0500

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => { 
+    return (
+        <nav className="navbar navbar-primary bg-ligth bg-gradient">
+            <div className="container-fluid">
+                <div class="position-absolute top-50 start-50 translate-middle">
+                    <span className="navbar-brand fs-4 fst-italic lh-lg text-success ">Taller Poo-Coddy Coello</span>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
